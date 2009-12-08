c3c930d858b722bd5976bafcd6cbae7d3e05cd95
Blake Smith <blakesmith0@gmail.com>
2009-12-08 13:57:58 -0600

    Add emud.erl this time

---
+++ src/emud.erl
@@ -0,0 +1,16 @@
+-module(emud).
+-behaviour(application).
+-export([start/0, start/1, start/2, stop/1]).
+
+start(_Type, Args) ->
+	gen_server:start_link({local, emud}, core, [Args], []).
+
+start(Type) ->
+	start(Type, {port, 3333}).
+
+start() ->
+	start(normal, {port, 3333}).
+
+stop(_State) ->
+	gen_server:call(emud, stop).
+
