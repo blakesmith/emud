381c59b5df4fd05506b44974a3aede4469841e09
Blake Smith <blakesmith0@gmail.com>
2009-12-08 10:11:57 -0600

    Server is OTP compliant

--- src/core.erl
+++ src/core.erl
@@ -1,20 +1,30 @@
 -module(core).
--export([listen/1]).
+-behaviour(gen_server).
+-export([listen/1, stop/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+		terminate/2, code_change/3]).
 
 -include("client.hrl").
 
 -define(GEN_TCP_OPTIONS, [list, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
 listen(Port) ->
+	gen_server:start_link({local, emud}, ?MODULE, [Port], []).
+
+stop() ->
+	gen_server:call(?MODULE, stop).
+
+init([Port]) ->
 	io:fwrite("Emud started. Listening on port ~w...~n", [Port]),
-	register(client_manager, spawn(fun() -> manage_clients([]) end)),
+	register(emud_client_manager, spawn(fun() -> manage_clients([]) end)),
 	{ok, LSocket} = gen_tcp:listen(Port, ?GEN_TCP_OPTIONS),
-	do_accept(LSocket).	
+	register(emud_listener, spawn(fun() -> do_accept(LSocket) end)),	
+	{ok, listening}.
 
 do_accept(LSocket) ->
 	{ok, Socket} = gen_tcp:accept(LSocket),
 	spawn(fun() -> handle_client(Socket) end),
-	client_manager ! {connect, Socket},
+	emud_client_manager ! {connect, Socket},
 	do_accept(LSocket).
 
 handle_client(Socket) ->
@@ -23,7 +33,7 @@ handle_client(Socket) ->
 			gen_tcp:send(Socket, input_parse(Packet)),
 			handle_client(Socket);
 		{error, _Reason} ->
-			client_manager ! {disconnect, Socket}
+			emud_client_manager ! {disconnect, Socket}
 	end.
 
 manage_clients(Clients) ->
@@ -40,3 +50,17 @@ manage_clients(Clients) ->
 input_parse(Packet) ->
 	Packet.	
 
+handle_call(stop, _From, Tab) ->
+	{stop, normal, stopped, Tab}.
+
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
