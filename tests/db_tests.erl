e9151b6b1086698029c8bbe918d51729e1293351
Blake Smith <blakesmith0@gmail.com>
2009-12-12 11:41:00 -0600

    DB insert test

--- tests/db_tests.erl
+++ tests/db_tests.erl
@@ -2,14 +2,26 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TEST_TABLE, items).
+-define(TEST_TABLE_FIELDS, [name, power]).
+-record(items, {name, power}).
 
 setup() ->
 	db:init().
 
 teardown() ->
-	mnesia:delete_table(?TEST_TABLE).
+	mnesia:delete_table(?TEST_TABLE),
+	mnesia:stop().
 
 create_ram_table_test() ->
 	setup(),
-	?assertEqual({atomic, ok}, db:create_ram_table(?TEST_TABLE, [name, power])),
+	?assertEqual({atomic, ok}, db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS)),
 	teardown().
+
+insert_test() ->
+	setup(),
+	db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS),
+	R = #items{name=sword, power=10},
+	?assertEqual({atomic, ok}, db:insert(R)),
+	teardown().
+	
+
