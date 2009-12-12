b3539a14b8f3f5cc6a03d285292b96efa69058cc
Blake Smith <blakesmith0@gmail.com>
2009-12-12 12:17:32 -0600

    Wrap all test functions in nested funs

--- tests/db_tests.erl
+++ tests/db_tests.erl
@@ -13,22 +13,28 @@ teardown() ->
 	mnesia:delete_table(?TEST_TABLE),
 	mnesia:stop().
 
-create_ram_table_test() ->
-	setup(),
-	?assertEqual({atomic, ok}, db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS)),
-	teardown().
+create_ram_table_test_() ->
+	fun() ->
+		setup(),
+		?assertEqual({atomic, ok}, db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS)),
+		teardown()
+	end.
 
-insert_test() ->
-	setup(),
-	db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS),
-	R = #items{name=sword, power=10},
-	?assertEqual({atomic, ok}, db:insert(R)),
-	teardown().
+insert_test_() ->
+	fun() ->
+		setup(),
+		db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS),
+		R = #items{name=sword, power=10},
+		?assertEqual({atomic, ok}, db:insert(R)),
+		teardown()
+	end.
 	
-select_test() ->
-	setup(),
-	db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS),
-	R = #items{name=sword, power=10},
-	db:insert(R),
-	?assertEqual([#items{name=sword, power=10}], db:select(qlc:q([X || X <- mnesia:table(items)]))),
-	teardown().
+select_test_() ->
+	fun() ->
+		setup(),
+		db:create_ram_table(?TEST_TABLE, ?TEST_TABLE_FIELDS),
+		R = #items{name=sword, power=10},
+		db:insert(R),
+		?assertEqual([#items{name=sword, power=10}], db:select(qlc:q([X || X <- mnesia:table(items)]))),
+		teardown()
+	end.
--- tests/input_parser_tests.erl
+++ tests/input_parser_tests.erl
@@ -1,5 +1,7 @@
 -module(input_parser_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-parse_test() ->
-	?assertEqual("test", input_parser:parse("test")).
+parse_test_() ->
+	fun() ->
+		?assertEqual("test", input_parser:parse("test"))
+	end.
