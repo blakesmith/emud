a16569a79f9b0781fe6323835ac4f6ba34d74325
Blake Smith <blakesmith0@gmail.com>
2009-12-16 17:23:53 -0600

    Auth init and add_user

--- src/auth.erl
+++ src/auth.erl
@@ -1,6 +1,18 @@
 -module(auth).
 -include_lib("eunit/include/eunit.hrl").
--export([init/0]).
+-include("users.hrl").
+-export([init/0, add_user/3]).
 
 init() ->
-	ok.
+	crypto:start(),
+	db:create_disc_table(users, [login, password, email]).
+
+add_user(Login, Password, Email) ->
+	P1 = crypto:sha(Password),
+	U = #users{login=Login, password=P1, email=Email},
+	case db:insert(U) of
+		{atomic, ok} ->
+			U;	
+		{aborted, Reason} ->
+			{error, Reason}
+	end.
--- tests/auth_tests.erl
+++ tests/auth_tests.erl
@@ -1,2 +1,34 @@
 -module(auth_tests).
 -include_lib("eunit/include/eunit.hrl").
+-include("../src/users.hrl").
+
+setup() ->
+	db:init().
+
+teardown(_Value) ->
+	mnesia:delete_table(users).
+
+init_test_() ->
+	{"Creates a disc auth table if it doesn't exist",
+		setup, fun setup/0, fun teardown/1,
+		fun() ->
+				% First time, it shouldn't exist
+				?assertEqual({atomic, ok}, auth:init()),
+				% Second time, it should exist
+				?assertEqual({aborted, {already_exists,users}}, auth:init())
+		end
+	}.
+
+add_user_test_() ->
+	{"Inserts a new user into the users table",
+		setup, fun setup/0, fun teardown/1,
+		fun() ->
+				crypto:start(),
+				auth:init(),
+				L = "blake",
+				P = "test",
+				E = "bob@bob.com",
+				ExpectedUser = #users{login=L, password=crypto:sha(P), email=E},
+				?assertEqual(ExpectedUser, auth:add_user(L, P, E))
+		end
+	}.
